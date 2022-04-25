e9d170e43f3b88f8270209360b2dcddbe2a01742
gurusabarish <gurusabarisha@gmail.com>
2022-04-25 19:24:02 +0530

    update

--- src/main/java/com/example/jobfinderbackend/controller/ApplicationController.java
+++ src/main/java/com/example/jobfinderbackend/controller/ApplicationController.java
@@ -8,6 +8,7 @@ import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
+@CrossOrigin(origins = "*")
 @RestController
 @RequestMapping("api/v1/application")
 public class ApplicationController {
